a48a31970a91464fea1d24b5b906847b23d73d95
arthur17071992 <arthur.1707@yahoo.com.vn>
2018-05-29 22:09:27 -0400

    Add files via upload

---
+++ Sequential File Output, Simplified/src/UsingPrinterWriter.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class UsingPrinterWriter {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		PrintWriter outputFile = new PrintWriter("Arthur Nguyen.txt"); //use your name here!
+		outputFile.println("Hello from Arthur Nguyen");
+		outputFile.close();
+//		System.out.println("all done!");
+
+	}
+
+}
